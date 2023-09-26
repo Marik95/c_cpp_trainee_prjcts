8d08a9d62b4ce098ddf5c6a73c85a145876ffbf9
marik95 <alexmarchenko70@gmail.com>
2023-09-26 17:59:44 +0300

    first meetting with statements and functions

--- chapter1/trainee_prgm.cpp
+++ chapter1/trainee_prgm.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 
-
 using namespace std;
 
+
+// function
+int diffFunc(int mainNumb, int smallNumb){
+    
+    int diff = mainNumb - smallNumb;
+
+    return diff;
+}
+
 int main(){
-    cout << "Hello twice" << endl;
+    
+    //statements output
+    int firstNumber {3};
+    int secondNumber {5};
+    int sum  = firstNumber + secondNumber;
+
+    cout << "statements output is: " << sum << endl;
+    
+    // function output
+    int diff = diffFunc(secondNumber, firstNumber);
+
+    cout << "function output is: " << diff << endl;
+    
     return 0;
 }
